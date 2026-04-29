f4a1186f12858414b868ff9af052e63f658aee25
hyungi <hy46@tardistechnology.com>
2026-04-29 16:27:19 +0900

    chore: 번들 ID com.tardistechnology → com.yogurtc로 변경 (Android/iOS 공통)
    - android/app/build.gradle.kts: namespace 및 applicationId 변경
    - ios/Runner.xcodeproj/project.pbxproj: PRODUCT_BUNDLE_IDENTIFIER 변경 (Runner, RunnerTests)
    - MainActivity.kt: com/tardistechnology → com/yogurtc 패키지 경로로 이동

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.tardistechnology.guitar_tuner"
+    namespace = "com.yogurtc.guitar_tuner"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.tardistechnology.guitar_tuner"
+        applicationId = "com.yogurtc.guitar_tuner"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
